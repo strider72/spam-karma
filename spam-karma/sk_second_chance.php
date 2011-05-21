aeba063daceed2b01e2dbb9ce9962a47cf6d953c
google_com@striderweb.com <google_com@striderweb.com@95db00fb-ea51-0410-9273-b741f0d80fca>
2011-05-21 14:41:21 +0000

    * Partial fix for "Conflict with Bad Behavior" bug. * Code still needs work.  It calls wp-config directly and should not.

--- spam-karma/sk_second_chance.php
+++ spam-karma/sk_second_chance.php
@@ -1,6 +1,6 @@
 <?php
 /**********************************************************************************************
- Spam Karma (c) 2009 - http://code.google.com/p/spam-karma/
+ Spam Karma (c) 2011 - http://code.google.com/p/spam-karma/
 
  This program is free software; you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
@@ -12,11 +12,13 @@
   GNU General Public License for more details.
 
 ************************************************************************************************/
-?><html><head /><body>
-<?php
+
+//FIXME: WE should never call wp-config.php directly!
 require_once('../../../wp-config.php');
 global $sk_log;
-include_once(dirname(__FILE__) . "/sk_core_class.php");
+echo '<html><head></head><body>';
+
+include_once(dirname(__FILE__) . '/sk_core_class.php');
 
 $comment_ID = (int) @$_REQUEST['c_id'];
 $author_email = @$_REQUEST['c_author'];
@@ -24,21 +26,21 @@ $author_email = @$_REQUEST['c_author'];
 //DEBUG:
 $sk_log->live_output = false;
 
-$sk_log->log_msg(__("Second Chance. Comment ID:", 'spam-karma') . $comment_ID, 4, $comment_ID, "2nd_chance");
+$sk_log->log_msg(__('Second Chance. Comment ID:', 'spam-karma') . $comment_ID, 4, $comment_ID, '2nd_chance');
 $sk_core = new SK_Core(0, true, false);
 
 if ($sk_core->load_comment($comment_ID))
 {
-	//echo "<pre>"; 	print_r($sk_core->cur_comment);
+	//echo '<pre>'; 	print_r($sk_core->cur_comment);
 	if ($sk_core->cur_comment->author_email != $author_email)
-		die(__("Email not matching comment ID", 'spam-karma'));
+		die(__('Email not matching comment ID', 'spam-karma'));
 		
 	$sk_core->load_plugin_files();	
 	$sk_core->second_chance();
 }
 else
 {
-	die(__("Invalid comment", 'spam-karma'));
+	die(__('Invalid comment', 'spam-karma'));
 }
 ?>
 </body></html>
